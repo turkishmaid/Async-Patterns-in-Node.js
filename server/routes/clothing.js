abfa1e78c31a2e439f6b7706f964fd5740d541c8
Sara Ziner <sara.ziner@googlemail.com>
2021-02-14 11:58:19 +0100

    an awaiting get() // BAD

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -9,8 +9,8 @@ let hurz = "nix"
 // asymc wraps in Promise
 async function getClothingData() {
     // 4. await
-    let rawData = await fsp.readFile(datafile)  // await unpacks Promise
-    return JSON.parse(rawData.toString("utf-8"))
+    let rawData = await fsp.readFile(datafile, "utf-8")  // await unpacks Promise
+    return JSON.parse(rawData.toString())
 
     // 3. Promis
     // return new Promise((resolve, reject) => {
@@ -25,47 +25,65 @@ async function getClothingData() {
     // })
 }
 
-/* GET all clothing */
+// 5. an awaiting get()
 router.route('/')
-    .get(function (req, res) {
-        // 3. Promise
-        getClothingData()
-            // 1st
-            .then(data => {
-                hurz = "then"
-                console.log("serving clothing data")
-                res.send(data)
-            })
-            // 3rd -> nein, 2nd -- nur die Ausgabe erfolgt verspätet
-            .catch(err => {
-                hurz = "catch"
-                console.error(err)
-                //res.status(500).send(err)  <-- don't do that. Not even in trainings.
-                res.status(500).send("Sorry.")
-            })
-            // 2nd
-            .finally(() => {
-                console.log(`finally done. ${hurz}`)  // druckt im Fehlerfall "catch"
-            })
+    .get(async function (req, res) {
 
-        // 2. callback
-        // let clothngData = getClothingData((err, data) => {
-        //     if (err) {
-        //         console.error(err)
-        //     } else {
-        //         console.log("serving clothing data")
-        //         res.send(data)
-        //
-        //     }
-        // });
-
-        // 1. syncronous
-        // let rawData = fs.readFileSync(datafile, 'utf8');
-        // let clothingData = JSON.parse(rawData);
-        // console.log("returning clothing data")
-        // res.send(clothingData);
+        // this is NOT good in general, because it blocks the event loop
+        try {
+            let data = await getClothingData()
+            console.log("serving clothing data")
+            res.send(data)
+        } catch (err) {
+            console.error(err)
+            res.status(500).send("Sorry.")
+        }
 
         console.log("doing more work")
     });
+
+
+/* GET all clothing */
+// router.route('/')
+//     .get(function (req, res) {
+//         // 3. Promise
+//         getClothingData()
+//             // 1st
+//             .then(data => {
+//                 hurz = "then"
+//                 console.log("serving clothing data")
+//                 res.send(data)
+//             })
+//             // 3rd -> nein, 2nd -- nur die Ausgabe erfolgt verspätet
+//             .catch(err => {
+//                 hurz = "catch"
+//                 console.error(err)
+//                 //res.status(500).send(err)  <-- don't do that. Not even in trainings.
+//                 res.status(500).send("Sorry.")
+//             })
+//             // 2nd
+//             .finally(() => {
+//                 console.log(`finally done. ${hurz}`)  // druckt im Fehlerfall "catch"
+//             })
+//
+//         // 2. callback
+//         // let clothngData = getClothingData((err, data) => {
+//         //     if (err) {
+//         //         console.error(err)
+//         //     } else {
+//         //         console.log("serving clothing data")
+//         //         res.send(data)
+//         //
+//         //     }
+//         // });
+//
+//         // 1. syncronous
+//         // let rawData = fs.readFileSync(datafile, 'utf8');
+//         // let clothingData = JSON.parse(rawData);
+//         // console.log("returning clothing data")
+//         // res.send(clothingData);
+//
+//         console.log("doing more work")
+//     });
 
 module.exports = router;
